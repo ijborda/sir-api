4691fbcbf236d6a755bcb57c2d21b806f505de19
Imari Joy Borda <imari.borda2018@gmail.com>
2022-06-03 00:05:46 +0800

    Eslint refactor

--- server.js
+++ server.js
@@ -5,24 +5,24 @@ const Big = require('big.js');
 // Create server
 const app = express();
 // eslint-disable-next-line no-undef
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8000;
 
 // Define server
 app.get('/api/sir', (req, res) => {
-
   // Get parameters
-  const t = Big(req.query.t)
-  const sInit = Big(req.query.s)
-  const iInit = Big(req.query.i)
-  const rInit = Big(req.query.r)
-  const n = Big(sInit.plus(iInit).plus(rInit))
-  const beta = Big(req.query.b)
-  const gamma = Big(req.query.g)
+  const t = Big(req.query.t);
+  const sInit = Big(req.query.s);
+  const iInit = Big(req.query.i);
+  const rInit = Big(req.query.r);
+  const n = Big(sInit.plus(iInit).plus(rInit));
+  const beta = Big(req.query.b);
+  const gamma = Big(req.query.g);
 
-  let sir = []
-  let sirCurrent = {}
+  const sir = [];
+  let sirCurrent = {};
 
-  for (let j = 0; j < Number(t); j++ ) {
+  // eslint-disable-next-line no-plusplus
+  for (let j = 0; j < Number(t); j++) {
     if (j === 0) {
       // Initial values
       sirCurrent = {
@@ -30,38 +30,40 @@ app.get('/api/sir', (req, res) => {
         s: +sInit,
         i: +iInit,
         r: +rInit,
-      }
+      };
     } else {
       // Compute derivatives
-      let sDelta = beta.times(-1).times(sir[j-1].s).times(+sir[j-1].i).div(n) 
-      let iDelta = (beta.times(sir[j-1].s).times(+sir[j-1].i).div(n)).minus(gamma.times(+sir[j-1].i))
-      let rDelta = gamma.times(+sir[j-1].i)
+      const sDelta = beta.times(-1).times(sir[j - 1].s).times(+sir[j - 1].i).div(n);
+      const iDelta = sDelta.times(-1).minus(gamma.times(+sir[j - 1].i));
+      const rDelta = gamma.times(+sir[j - 1].i);
       // Compute current SIR
-      let sCurrent = +sir[j-1].s + +sDelta
-      let iCurrent = +sir[j-1].i + +iDelta
-      let rCurrent = +sir[j-1].r + +rDelta
+      const sCurrent = +sir[j - 1].s + +sDelta;
+      const iCurrent = +sir[j - 1].i + +iDelta;
+      const rCurrent = +sir[j - 1].r + +rDelta;
       // Append current SIR to SIR array
       sirCurrent = {
         t: j,
         s: sCurrent,
         i: iCurrent,
         r: rCurrent,
-      }
+      };
     }
-    sir.push(sirCurrent)
+    sir.push(sirCurrent);
   }
 
   // Round to two decimal places
-  let sirRounded = sir.map(a => {
-    return {
+  const sirRounded = sir.map((a) => {
+    const round = {
       t: +a.t.toFixed(2),
       s: +a.s.toFixed(2),
       i: +a.i.toFixed(2),
       r: +a.r.toFixed(2),
-    }
-  })
+    };
+    return round;
+  });
   res.json(sirRounded);
-})
+});
 
 // Listen to PORT
-app.listen(PORT, () => console.log(`Server Running on ${PORT}`))
+// eslint-disable-next-line no-console
+app.listen(PORT, () => console.log(`Server Running on ${PORT}`));
